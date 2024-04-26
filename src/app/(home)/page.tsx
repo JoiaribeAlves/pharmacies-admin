3380d25146fb1f8f10f80c7d99ad323af1877c35
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-25 22:27:07 -0400

    feat: clear project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Página inicial"
+};
+
+function Home() {
+  return <h1>Hello, world</h1>
+}
+
+export default Home;
