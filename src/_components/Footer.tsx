817e3dd3048116f5d5f8aa26fe822bdbd0d8325d
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-26 01:34:18 -0400

    feat: add CopyrightIcon to component

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,11 +1,13 @@
 import { getYear } from 'date-fns'
+import { CopyrightIcon } from 'lucide-react'
 
 function Footer() {
   const year = getYear(new Date())
 
   return (
     <footer className="flex h-16 items-center justify-center bg-red-600 text-sm text-white">
-      &copy; {year} - Todos os direitos reservados
+      <CopyrightIcon size={13} />
+      &nbsp;{year} - Todos os direitos reservados
     </footer>
   )
 }
