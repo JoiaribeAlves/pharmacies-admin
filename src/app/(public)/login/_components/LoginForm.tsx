5541ce8e8313dde5af044f037ebdb23c982f49b6
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-28 18:42:29 -0400

    feat: add validation to LoginForm with zod

--- src/app/(public)/login/_components/LoginForm.tsx
+++ src/app/(public)/login/_components/LoginForm.tsx
@@ -1,24 +1,46 @@
 'use client'
 
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Loader2 } from 'lucide-react'
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormMessage,
+} from '@/_components/ui/form'
+import { Input } from '@/_components/ui/input'
+import { Button } from '@/_components/ui/button'
+
+const loginFormSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email é obrigatório',
+    })
+    .email('Email inválido')
+    .toLowerCase(),
+  password: z.string({
+    required_error: 'Senha é obrigatória',
+  }),
+})
 
 function LoginForm() {
   const [loginFailure, setLoginFailure] = useState(false)
 
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+  })
+
   const router = useRouter()
 
-  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const onSubmit = async (data: z.infer<typeof loginFormSchema>) => {
     setLoginFailure(false)
-
-    const formData = new FormData(event.currentTarget)
-
-    const data = {
-      email: formData.get('email'),
-      password: formData.get('password'),
-    }
 
     const response = await signIn('credentials', {
       ...data,
@@ -35,31 +57,66 @@ function LoginForm() {
 
   return (
     <div>
-      <form onSubmit={handleLogin} className="flex flex-col gap-4">
-        <input
-          type="email"
-          name="email"
-          placeholder="Digite seu email"
-          className="rounded-lg border-0 bg-white p-3 text-gray-800 outline-none ring-0 placeholder:text-gray-400"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Digite sua senha"
-          className="rounded-lg border-0 bg-white p-3 text-gray-800 outline-none ring-0 placeholder:text-gray-400"
-        />
-        <button
-          type="submit"
-          className="rounded-lg bg-red-600 p-3 text-white transition-colors hover:bg-red-700"
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-6"
         >
-          Entrar
-        </button>
-      </form>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Digite seu email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Digite sua senha"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            disabled={form.formState.isSubmitting}
+            className="w-full"
+          >
+            {form.formState.isSubmitting ? (
+              <>
+                <Loader2 className="h-4 w-4 animate-spin" />
+                Aguarde...
+              </>
+            ) : (
+              <>Entrar</>
+            )}
+          </Button>
+        </form>
+      </Form>
 
       {loginFailure && (
-        <span className="mt-4 block rounded-lg bg-red-200 p-2 text-center text-xs text-red-800">
+        <div className="mt-4 rounded-lg bg-red-200 p-2 text-center text-xs text-red-800">
           Email e Senha não conferem
-        </span>
+        </div>
       )}
     </div>
   )
--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from './_components/LoginForm'
 
 function Page() {
   return (
-    <div className="mx-4 flex w-full max-w-md flex-col gap-6 rounded-lg bg-gray-100 p-4">
+    <div className="mx-4 flex w-full max-w-md flex-col gap-6 rounded-lg bg-gray-100 p-6">
       <h1 className="text-center text-2xl font-bold text-gray-800">
         Fazer login
       </h1>
