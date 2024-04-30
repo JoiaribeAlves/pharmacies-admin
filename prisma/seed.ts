3e05bfe449768f776b3490aa18de1bb17396e46e
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-29 20:34:14 -0400

    feat: add seed script to create pharmacies and addresses

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,32 +1,285 @@
-import { genSalt, hash } from 'bcrypt-ts'
+// import { genSalt, hash } from 'bcrypt-ts'
 
 import { prismaClient } from '@/lib/prisma'
 
-async function user(name: string, email: string, password: string) {
-  const salt = await genSalt(10)
-  const passwordHash = await hash(password, salt)
+// async function user(name: string, email: string, password: string) {
+//   const salt = await genSalt(10)
+//   const passwordHash = await hash(password, salt)
 
+//   try {
+//     const user = await prismaClient.user.findUnique({
+//       where: {
+//         email,
+//       },
+//     })
+
+//     if (user) {
+//       console.error('Usuário já existe!')
+//       return
+//     }
+
+//     await prismaClient.user.create({
+//       data: {
+//         name,
+//         email,
+//         password: passwordHash,
+//       },
+//     })
+
+//     console.log('Usuário criado com sucesso!')
+//   } catch (error) {
+//     console.error(error)
+//   } finally {
+//     prismaClient.$disconnect()
+//   }
+// }
+
+// user('Joiaribe', 'teste@teste.com.br', 'abcd1234')
+
+async function newPharmacy() {
   try {
-    const user = await prismaClient.user.findUnique({
-      where: {
-        email,
+    const pharmacies = [
+      {
+        name: 'RD Farma',
+        phones: ['69992931044', '69992931044'],
+        imageUrl: '',
       },
-    })
+      {
+        name: 'Farmácia Mini Preço',
+        phones: ['6935216744'],
+        imageUrl: '',
+      },
+      {
+        name: 'Drogasil',
+        phones: ['69996067132', '69996067132'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Baixo Preço',
+        phones: ['6935211572'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Bernardo',
+        phones: ['69993074455', '69993074455'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Mais Saúde',
+        phones: ['6935211810', '69992525658'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmadroga',
+        phones: ['6935212506'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Santa Lúcia',
+        phones: ['6935212520'],
+        imageUrl: '',
+      },
+      {
+        name: 'Drogaria Ultra Popular',
+        phones: ['6935214723', '69993042139'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Economize',
+        phones: ['6935211460'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Mini Preço',
+        phones: ['6935211460'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Preço Baixo',
+        phones: ['6935212222', '69993679463'],
+        imageUrl: '',
+      },
+      {
+        name: 'Drogaria Líder',
+        phones: ['6935212635'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Unifarma',
+        phones: ['6935214138'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Farminas',
+        phones: ['6935213020', '69993333020'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Pague Menos',
+        phones: ['6935216708', '69981600011'],
+        imageUrl: '',
+      },
+      {
+        name: 'Farmácia Preço Baixo',
+        phones: ['6935213281'],
+        imageUrl: '',
+      },
+      {
+        name: 'Drogaria Ultra Popular',
+        phones: ['6935213874'],
+        imageUrl: '',
+      },
+    ]
 
-    if (user) {
-      console.error('Usuário já existe!')
-      return
+    const addresses = [
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1528',
+        district: 'Setor 2',
+        complement: 'Ao lado do supermercado Ebenezer',
+        mapUrl: 'https://bit.ly/nW2Fq7Y',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '2568',
+        district: 'Setor 5',
+        complement: 'Em frente ao supermercado IG',
+        mapUrl: 'https://bit.ly/6vP1rLX',
+      },
+      {
+        street: 'Rua Belo Horizonte',
+        number: '3055',
+        district: 'Setor 5',
+        complement: 'Sob esquina com Av. Dom Pedro I',
+        mapUrl: 'https://bit.ly/aD4Oe8S',
+      },
+      {
+        street: 'Av. Florianópolis',
+        number: '1735',
+        district: 'Setor 7',
+        complement: 'Em frente ao hospital municipal',
+        mapUrl: 'https://bit.ly/4jK8ZbV',
+      },
+      {
+        street: 'Av. Marechal Rondon',
+        number: '2915',
+        district: 'Setor 1',
+        complement: 'Em frente ao supermercado TaíMax',
+        mapUrl: 'https://bit.ly/pX9Mn5A',
+      },
+      {
+        street: 'Av. Florianópolis',
+        number: '1719',
+        district: 'Setor 7',
+        complement: 'Em frente ao hospital municipal',
+        mapUrl: 'https://bit.ly/3T6c2yJ',
+      },
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1954',
+        district: 'Setor 1',
+        complement: 'Próximo a loja avenida',
+        mapUrl: 'https://bit.ly/9uQ5Cf1',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '2552',
+        district: 'Setor 5',
+        complement: 'Em frente ao supermercado IG',
+        mapUrl: 'https://bit.ly/ASrtn3g',
+      },
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1623',
+        district: 'Setor 1',
+        complement: 'Sob esquina com Av. Marechal Rondon',
+        mapUrl: 'https://bit.ly/49OqfR0',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '2616',
+        district: 'Setor 5',
+        complement: 'Próximo ao supermercado IG',
+        mapUrl: 'https://bit.ly/49UIhkI',
+      },
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1544',
+        district: 'Setor 2',
+        complement: 'Ao lado da livraria Kassula',
+        mapUrl: 'https://bit.ly/4aGZY8r',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '2568',
+        district: 'Setor 5',
+        complement: 'Em frente ao supermercado IG',
+        mapUrl: 'https://bit.ly/3UrfkIk',
+      },
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1574',
+        district: 'Setor 2',
+        complement: 'Em frente a rodoviária dos colonos',
+        mapUrl: 'https://bit.ly/3UrcaEp',
+      },
+      {
+        street: 'Av. JK',
+        number: '1181',
+        district: 'Setor 3',
+        complement: 'Em baixo do Cassis Hotel',
+        mapUrl: 'https://bit.ly/3UujJul',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '3056',
+        district: 'Setor 5',
+        complement: 'Em frente a rodoviária municipal',
+        mapUrl: 'https://bit.ly/3WeqIc9',
+      },
+      {
+        street: 'Av. Pe. Adolpho Rohl',
+        number: '1484',
+        district: 'Setor 2',
+        complement: 'Ao lado da lotérica avenida',
+        mapUrl: 'https://bit.ly/3QcugI6',
+      },
+      {
+        street: 'Av. Marechal Rondon',
+        number: '2949',
+        district: 'Setor 1',
+        complement: 'Ao lado da M Garcia',
+        mapUrl: 'https://bit.ly/3Js8nAr',
+      },
+      {
+        street: 'Av. Dom Pedro I',
+        number: '2484',
+        district: 'Setor 5',
+        complement: 'Sob esquina com rua Ceará',
+        mapUrl: 'https://bit.ly/3w4AZg9',
+      },
+    ]
+
+    try {
+      pharmacies.map(async (pharmacy, index) => {
+        const res = await prismaClient.pharmacy.create({
+          data: pharmacy,
+        })
+
+        await prismaClient.address.create({
+          data: {
+            ...addresses[index],
+            pharmacyId: res.id,
+          },
+        })
+      })
+
+      console.log('Farmácias criadas com sucesso!')
+    } catch (error) {
+      console.error(error)
+    } finally {
+      await prismaClient.$disconnect()
     }
-
-    await prismaClient.user.create({
-      data: {
-        name,
-        email,
-        password: passwordHash,
-      },
-    })
-
-    console.log('Usuário criado com sucesso!')
   } catch (error) {
     console.error(error)
   } finally {
@@ -34,4 +287,4 @@ async function user(name: string, email: string, password: string) {
   }
 }
 
-user('Joiaribe', 'teste@teste.com.br', 'abcd1234')
+newPharmacy()
