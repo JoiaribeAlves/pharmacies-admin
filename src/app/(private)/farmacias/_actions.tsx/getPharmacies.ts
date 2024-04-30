e38c019f7d822b9dde93f2db0abb4a36c944bfb6
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-29 21:52:07 -0400

    feat: add try catch

--- src/app/(private)/farmacias/_actions.tsx/getPharmacies.ts
+++ src/app/(private)/farmacias/_actions.tsx/getPharmacies.ts
@@ -1,28 +1,34 @@
 import { prismaClient } from '@/lib/prisma'
 
 async function getPharmacies() {
-  const pharmacies = await prismaClient.pharmacy.findMany({
-    select: {
-      id: true,
-      name: true,
-      imageUrl: true,
-      phones: true,
-      address: {
-        select: {
-          street: true,
-          number: true,
-          district: true,
-          complement: true,
-          mapUrl: true,
+  try {
+    const pharmacies = await prismaClient.pharmacy.findMany({
+      select: {
+        id: true,
+        name: true,
+        imageUrl: true,
+        phones: true,
+        address: {
+          select: {
+            street: true,
+            number: true,
+            district: true,
+            complement: true,
+            mapUrl: true,
+          },
         },
       },
-    },
-    orderBy: {
-      name: 'asc',
-    },
-  })
+      orderBy: {
+        name: 'asc',
+      },
+    })
 
-  return pharmacies
+    return pharmacies
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await prismaClient.$disconnect()
+  }
 }
 
 export { getPharmacies }
