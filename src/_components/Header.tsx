c1f03e45df0180f64c29f78ae557ba682c0c920a
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-04-29 20:12:13 -0400

    style: add border bottom

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="bg-primary text-primary-foreground fixed left-72 right-0 top-0 h-16"></header>
+    <header className="bg-primary text-primary-foreground border-b-background fixed left-72 right-0 top-0 z-10 h-16 border-b"></header>
   )
 }
 
