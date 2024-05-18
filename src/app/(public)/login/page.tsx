213ec65067eb6c7854e3388e540603cfd04a51f0
Joiaribe G. Alves <joiaribegalves@protonmail.com>
2024-05-18 18:50:00 -0400

    feat: add page title and rename component from Page to LoginPage

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,6 +1,12 @@
+import type { Metadata } from 'next'
+
 import { LoginForm } from './_components/LoginForm'
 
-function Page() {
+export const metadata: Metadata = {
+  title: 'Fazer login | Plantão de farmácia',
+}
+
+function LoginPage() {
   return (
     <div className="mx-4 flex w-full max-w-md flex-col gap-6 rounded-lg bg-gray-100 p-6">
       <h1 className="text-center text-2xl font-bold text-gray-800">
@@ -12,4 +18,4 @@ function Page() {
   )
 }
 
-export default Page
+export default LoginPage
